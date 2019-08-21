bd28cdf231fdb2383721d8d36744743d0e6ce79a
ReadyH <35962311+ReadyH@users.noreply.github.com>
2019-08-21 17:00:12 +0800

    epoll 函数相关

---
+++ epoll.h
@@ -0,0 +1,16 @@
+
+#ifndef EPOLL_H
+#define EPOLL_H
+
+#include <sys/epoll.h>
+#include "threadpool.h"
+#include "http.h"
+
+#define MAX_EVENTS 1024
+
+int createEpoll(int flags);
+int addEpoll(int epollFd, int servSock, httpRequest* request, int events);
+int epollWait(int epollFd, struct epoll_event* events, int maxEvents, int timeout);
+int modifyEpoll(int epollFd, int servSock, httpRequest* request, int events);
+int handleEvents(int epollFd, int servSock, struct epoll_event* events, int eventsNum, char* path, struct threadPool* tp);
+#endif // EPOLL_H
